20d09a4e841c1399fed35df7400d5fbb2dc95620
KarenYaguapaz <32964453+KarenYaguapaz@users.noreply.github.com>
2017-10-27 12:34:09 -0500

    ejercicio

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <h1>test</h1>
+<?php
+function generar($l)
+{
+    $c= "abcdefghijklmnopqrstvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    $s= "";
+    for($i = 0; $i < $l; $i++)
+    {
+        $s .= $c[rand(0,strlen($c)-1)];
+    }
+    return $s;
+}
+ echo generar(50);
+?>
