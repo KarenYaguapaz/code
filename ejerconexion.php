c1b1e5647be0b079783d1f2dd5f445f8c9b45120
KarenYaguapaz <32964453+KarenYaguapaz@users.noreply.github.com>
2017-10-31 19:03:34 -0500

    intento numero 3 conexión

--- ejerconexion.php
+++ ejerconexion.php
@@ -1,21 +1,31 @@
 <?php
 
-if(!($conn = mysql_connect('servername', 'username', 'password')));
-{
-    exit();
  }
 else {
     mysql_select_db("code",$conn);
     $query = "selct nombre from tabla 1 where id=1";
 
-    $q = mysql_query($query, $conn);
+$conn = new mysqli($servername, $username, $password);
 
-    $result = mysql_result($q, 0);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sql = 'SELECT  emp_id, emp_name, emp_lastname FROM employee';
+$mysql_select_db('test_db');
+$retval = mysql_query ( $sql, $conn );
 
-    echo $result;
-
+if(! $retval ) {
+   die('Could not get data: ' . mysql_error());
 }
 
+while($row = mysql_fetch_assoc($retval)) {
+    echo  "EMP ID : {$row['emp_id']} <br> ".
+        "EMP USERNAME : {$row['emp_username']} <br> ".
+        "EMP LASTNAME : {$row['emp_lastname']} <br> ".
+        "--------------------------------<br>";
+}
+echo "Fetched data successfully\n";
+
+mysql_clase($conn);
+
 ?>
-
-
