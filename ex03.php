3e347a35dfdebb92fba44366ebcc731ad7cd5ece
KarenYaguapaz <32964453+KarenYaguapaz@users.noreply.github.com>
2017-11-13 14:59:30 -0500

    uso de integer y string

---
+++ ex03.php
@@ -0,0 +1,13 @@
+<h2>Ejercicio 3</h2>
+<?php
+//Crea dos variables cuyo nombre sea "uno" o "dos"
+
+// String
+$uno = "Contenido de la variable 1";
+
+// Integer
+$dos = 245;
+
+echo "UNO:" .$uno."<br/>";
+echo "DOS:" .$dos."<hr/>";
+?>
